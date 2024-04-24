df619fd9b5470371f5a3020418b03b81204f44e9
Lyshaka <52411658+Lyshaka@users.noreply.github.com>
2024-04-24 17:10:40 +0200

    Repaired everything (I hope)

---
+++ Assets/Scripts/EnemyTrack.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class EnemyTrack : MonoBehaviour
+{
+	[SerializeField] private SplineContainer spline;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
